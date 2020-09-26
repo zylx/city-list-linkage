05020095779b1c6ee93b729d23e7b0021310a254
zylx <549535805@qq.com>
2020-09-26 23:01:44 +0800

    增加字母导航栏组件，数据交互待完善

--- src/views/city/components/Alphabet.vue
+++ src/views/city/components/Alphabet.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>
-    alphabet
-  </div>
+  <ul class="alphabet-list">
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @click="handleLetterClick"
+    >
+      {{ item }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-  name: 'Alphabet'
+  name: 'Alphabet',
+  data () {
+    return {
+      letters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+      touchActive: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      console.log(e.target.innerText)
+    }
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+.alphabet-list {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  width: 0.5rem;
+
+  .item {
+    line-height: 0.4rem;
+    text-align: center;
+    color: #00bcd4;
+  }
+}
+</style>
