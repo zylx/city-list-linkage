4bd9cbded147f427f0650ec2129044ce966ab9b3
zylx <549535805@qq.com>
2020-09-26 22:34:24 +0800

    引入初始化样式文件及解决移动端1px边框问题样式文件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/style/reset.css' // css 样式初始化
+import './assets/style/border.css' // 解决移动端 1px 边框问题
+
 Vue.config.productionTip = false
 
 new Vue({
